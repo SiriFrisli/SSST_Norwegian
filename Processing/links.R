3e002e16ceb02260dc4f46927651a5dac4fe213e
SiriFrisli <siri.f@hotmail.com>
2024-09-15 14:58:15 +0200

    extract long urls

--- Processing/links.R
+++ Processing/links.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(httr)
 library(urltools)
 
-covid <- readRDS("D:/Data/Datasets/covid_processed_class99_rt.RDS")
+covid <- readRDS("E:/Data/Datasets/covid_processed_class99_rt.RDS")
 
 # Want to extract the external links and add two new column to the df:
 # 1. Column containing the full link
@@ -10,31 +10,26 @@ covid <- readRDS("D:/Data/Datasets/covid_processed_class99_rt.RDS")
 
 # Creating a new df where I remove any observation without any URLs
 # Then filter out non-external URLs
-
-
-
 covid_links <- covid |>
   drop_na(urls) |>
   unnest(urls) |> 
   select(-c(start, end, images, status, title, description, unwound_url, media_key))
 
-# Extracting the domains, want to find any url-shortener present
-covid_ext_dom <- sapply(covid_ext$expanded_url, function (i) {
-  x <- url_parse(i)
-  c(x$domain)
-  })
-
-covid_ext_dom <- data.frame(dom_url=covid_ext_dom,
-                            id=covid_ext$id)
-
 # Creating a new df, filtering out links to twitter, I only want to keep external links
 covid_ext <- covid_links %>%
   filter(!str_detect(display_url, "twitter.com"))
 
+# Extracting the domains, want to find any url-shortener present
+# covid_ext_dom <- sapply(covid_ext$expanded_url, function (i) {
+#   x <- url_parse(i)
+#   c(x$domain)
+# })
+# 
+# covid_ext_dom <- data.frame(dom_url=covid_ext_dom,
+#                             id=covid_ext$id)
 
 covid_ext_short <- covid_ext |>
   select(id, expanded_url)
-
 
 pattern <- c("bit.ly", "dlvr.it", "ift.tt", "ow.ly", "t.co", "buff.ly", "goo.gl", 
              "fal.cn", "zpr.io", "hubs.ly", "ms.spr.ly",  "fb.me", "qoo.ly", "is.gd",
@@ -51,28 +46,37 @@ new_dataframe <- covid_ext |>
 
 httr::set_config(config(ssl_verifypeer = FALSE, ssl_verifyhost = FALSE))
 expanded_urls <- new_dataframe$expanded_url
-out_df <- data.frame()
+num_urls <- length(expanded_urls)
+out_df <- data.frame(url = rep(NA, num_urls))
 
-for (i in 1:length(expanded_urls)) {
-  valid_url <- TRUE
-  while(valid_url){
-    tryCatch({
-      x <- HEAD(expanded_urls[i], timeout(10))  # Set timeout to 10 seconds
-      if (x$status_code == 200) {
-        out_df <- bind_rows(out_df, data.frame(url = x$url))
-        valid_url <- FALSE
-      } else {
-        out_df <- bind_rows(out_df, data.frame(url = "invalid url"))
-        valid_url <- FALSE
-      }
-    }, error = function(e) {
-      out_df <- bind_rows(out_df, data.frame(url = "invalid url"))
-      valid_url <- FALSE
-    })
-  }
+for (i in 1:num_urls) {
+  tryCatch({
+    # Attempt to get the HEAD response for each URL
+    response <- HEAD(expanded_urls[i], timeout(10))  # Set timeout to 10 seconds
+    
+    # Check the status code
+    if (response$status_code == 200) {
+      # Store the long URL in the result data frame
+      out_df$url[i] <- response$url
+    } else {
+      # Store 'invalid url' if status code is not 200
+      out_df$url[i] <- "invalid url"
+    }
+  }, error = function(e) {
+    # Store 'invalid url' in case of an error
+    out_df$url[i] <- "invalid url"
+    # Optional: log the error for troubleshooting
+    cat("Error with URL ", expanded_urls[i], ": ", e$message, "\n")
+  })
 }
 
-out_df <- data.frame()
-out <- covid_short |>
-  mutate(response = map(paste0(out_df, expanded_url), 
-                        safely(httr::GET), httr::timeout(3)))
+combined <- new_dataframe |>
+  cbind(out_df)
+
+saveRDS(combined, "E:/Data/Datasets/short_urls.RDS")
+
+# Add to the bigger dataset
+# Unnesting the list column first
+covid_unnested <- covid |>
+  unnest(urls, keep_empty = TRUE) |>
+  select(-c(start, end, images, status, display_url, title, description, unwound_url, media_key))
